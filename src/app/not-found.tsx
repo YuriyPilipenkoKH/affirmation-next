45daae6eec61853f9299085a8154ac15f45b4e5e
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-26 23:58:29 +0200

    404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+
+function NotFound() {
+    return (
+      <div className="flex gap-2  justify-center w-full   text-slate-300 text-2xl font-bold">
+      <h1>
+        Sorry Dude
+      </h1>
+      <Image src='/coyote-offline.png' alt='icon' width={200} height={200}/>
+      </div>
+    )
+  }
+  
+  export default NotFound
