1a28c579e53b2aa3c5182730ebdb089743ca1d2c
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-27 00:01:24 +0200

    NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ function NotFound() {
     return (
       <div className="flex gap-2  justify-center w-full   text-slate-300 text-2xl font-bold">
       <h1>
-        Sorry Dude
+        Sorry Dude 404
       </h1>
       <Image src='/coyote-offline.png' alt='icon' width={200} height={200}/>
       </div>
